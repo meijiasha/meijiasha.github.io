2bd9d14d0dd0c65951dce6ea93b4d79253c06ad3
4icsshu <meijiasha.tw@gmail.com>
2025-11-25 19:40:36 +0800

    chore: update deployment workflow for React app

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // Replace with '/repo-name/' if deploying to https://<USERNAME>.github.io/<REPO>/
   plugins: [react()],
   resolve: {
     alias: {
