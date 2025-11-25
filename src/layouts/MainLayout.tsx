8166dfff437f7d97773a19a015ec66f334507e07
4icsshu <meijiasha.tw@gmail.com>
2025-11-26 01:57:03 +0800

    fix: restrict mobile sidebar height to below navbar

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -87,7 +87,7 @@ export default function MainLayout() {
                     <div className="md:hidden">
                         {/* Right Sheet: Store List - Controlled by Navbar toggle via store */}
                         <Sheet open={isStoreListPanelOpen} onOpenChange={setStoreListPanelOpen} modal={false}>
-                            <SheetContent side="right" className="p-0 w-[85vw]">
+                            <SheetContent side="right" className="p-0 w-80 top-16 h-[calc(100vh-4rem)] border-t">
                                 <StoreListPanel stores={stores} />
                             </SheetContent>
                         </Sheet>
